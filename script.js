4a26baed614cf2c92611ffb4f04eeeefe302300f
Golboojamshidi <Golbou.jamshidi@gmail.com>
2025-05-06 13:43:55 -0400

    e8487fbac01d4b9b8bdcc833d4a32956

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = function () {
   if (!token) {
     loginBtn.style.display = 'block';
     loginBtn.onclick = () => {
-      const clientId = 'YOUR_CLIENT_ID';
+      const clientId = 'e8487fbac01d4b9b8bdcc833d4a32956';
       const redirectUri = window.location.origin + '/callback.html';
       const scopes = 'user-top-read';
       const url = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${scopes}&redirect_uri=${encodeURIComponent(redirectUri)}`;
